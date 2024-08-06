17272a001b8b8cc36ef1927f6e565baaa7462643
MaximVoronez <maxsemerikov2012@mail.ru>
2024-08-06 23:01:11 +0300

    Модуль 1. Задание "Переменные"

---
+++ модуль 1 Переменные.py
@@ -0,0 +1,10 @@
+number_of_tasks = 12  # кол-во курсов
+number_of_hours = 1.5  # кол-во часов
+course_name = 'Курс : Python'  # Название курса
+average_execution_time = 0.125  # Среднее время выполения работы
+print('Вычисление: 1.5/12 =', number_of_hours / number_of_tasks)
+print(course_name + ', всего задач:', str(number_of_tasks) + '.Затрачено часов', str(number_of_hours) +
+      '. Среднее время выполнения', average_execution_time, 'часа.')
+# Я переменные "number_of_tasks-12"  и 'number_of_hours-1.5' перевел в строку для того, чтобы
+# сразу поставить точку (12. и 1.5.). подругому выходило 12 . и 1.5 .
+# Другого решения незнаю.
